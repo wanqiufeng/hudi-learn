2c22252bb11fd429a93854ff0daabf2e8caaa5f5
niceshot <wanqiufeng@outlook.com>
2020-10-23 17:44:04 +0800

    spark同kafka达通。

---
+++ src/main/java/com/niceshot/hudi/KafkaTest.scala
@@ -0,0 +1,44 @@
+package com.niceshot.hudi
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+
+/**
+ * @author created by chenjun at 2020-10-23 17:18
+ *
+ */
+object KafkaTest {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setAppName("Simple Application")
+      .setMaster("local[3]")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    val ssc = new StreamingContext(conf, Seconds(60))
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "192.168.16.237:9092,192.168.16.236:9092",
+      "key.deserializer" -> classOf[org.apache.kafka.common.serialization.StringDeserializer],
+      "value.deserializer" -> classOf[org.apache.kafka.common.serialization.StringDeserializer],
+      "group.id" -> "use_a_separate_group_id_for_each_stream2",
+      "auto.offset.reset" -> "earliest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+    val topics = Array("test")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+    //stream.map(record => (record.key, record.value)).print(6)
+    stream.map(record => {
+      println("消费kafka数据")
+      println(record.toString)
+    }).count().print();
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
--- src/main/java/com/niceshot/hudi/TestHudi2.scala
+++ src/main/java/com/niceshot/hudi/TestHudi2.scala
@@ -15,6 +15,13 @@ import org.apache.hudi.config.HoodieWriteConfig
 import org.apache.hudi.config.HoodieWriteConfig._
 import org.apache.spark.sql.SaveMode._
 import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
 /**
  * @author created by chenjun at 2020-10-14 15:32
@@ -22,37 +29,94 @@ import org.apache.spark.sql.{SaveMode, SparkSession}
  */
 object TestHudi2 {
   def main(args: Array[String]): Unit = {
+
+    //第一次数据怎么批量导入
+    //用spark直接拉取对应表在hive中昨天的分区，将其导入到hudi
+    //先将对应的接到canal
+    //再将对应的表，通过spark或其它方式，全量拉取到hudi表
+    //由于canal先接。全量数据后拉。所以会出现以下几种情况
+      //拉取的数据是已更新后的，canal的消费再更新一次。没有问题，因为hudi是基于主键来的，只要主键没变，数据再更新一次也无妨
+      //拉取数据是已经删除后的数据，cannal再删一次，已经没有这个主键，存在，大不了删除不成功，数据还是一致的
+
+
+    //表结构变更怎么响应？
+    //hudi会自动扩展字段。hive这边可以将对应的表删掉，下一次数据会自动新建新的表结构
+
+    //binlog消费？
+    //接收canal数据，将其转化为一个完成json，并对应其操作类型，比如删除，upsert
+    //将该json包装成一个df并执行
+
+
+    //spark程序如何解决日志问题
+
+
+    //程序需要提供哪些参数？
+      //表名
+      //路径名
+      //应用名，应用面可以根据表名自动拼
+
+    val conf = new SparkConf()
+      .setAppName("Simple Application")
+      .setMaster("local[1]")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    val ssc = new StreamingContext(conf, Seconds(60))
+    val spark = SparkSession.builder().config(conf).getOrCreate();
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "192.168.16.237:9092,192.168.16.236:9092",
+      "key.deserializer" -> classOf[org.apache.kafka.common.serialization.StringDeserializer],
+      "value.deserializer" -> classOf[org.apache.kafka.common.serialization.StringDeserializer],
+      "group.id" -> "use_a_separate_group_id_for_each_stream",
+      "auto.offset.reset" -> "earliest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    val topics = Array("test")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    //stream.map(record => (record.key, record.value))
+    stream.map(record => {
+      println("消费kafka数据")
+      println(record.toString)
+    })
+
+
+
     //System.setProperty("hadoop.home.dir", "C:\\Users\\wanqi\\DevTools\\hadoop-dev")
     //加上述代码的原因：https://stackoverflow.com/questions/35652665/java-io-ioexception-could-not-locate-executable-null-bin-winutils-exe-in-the-ha
-    val spark = SparkSession.builder
-      .appName("Simple Application")
-      .master("local[2]")
-      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-      .getOrCreate()
-    val tableName = "hudi_trips_cow"
+
+    val tableName = "hudi_hive_test4"
     //val basePath = "file:/Users/apple/Temp/hudi_data"
-    val basePath = "hdfs://192.168.16.181:8020/hudi_test"
+    val basePath = "hdfs://192.168.16.181:8020/hudi_test4"
     val dataGen = new DataGenerator
     //val inserts = convertToStringList(dataGen.generateInserts(10))
-    val inserts = List("{\"ts\": 0.0, \"uuid\": \"7fa0bf0a-ed9a-4bf0-bd63-7b163d39781a\", \"rider\": \"rider-214\", \"driver\": \"driver-213\", \"begin_lat\": 0.4726905879569653, \"begin_lon\": 0.46157858450465483, \"end_lat\": 0.754803407008858, \"end_lon\": 0.9671159942018241, \"fare\": 34.158284716382845, \"partitionpath\": \"americas/brazil/sao_paulo\"}")
+    val inserts = List("{\n    \"volume\": 456506,\n    \"symbol\": \"FB\",\n    \"ts\": \"2018-08-31 09:30:00\",\n    \"month\": \"08\",\n    \"high\": 177.5,\n    \"low\": 176.465,\n    \"key\": \"FB_2018-08-31 09\",\n    \"year\": 2018,\n    \"date\": \"2018/08/31\",\n    \"close\": 176.83,\n    \"open\": 177.29,\n    \"day\": \"31\",\n \"add_clo\": \"haha\"}",
+    "{\"volume\": 0, \"symbol\": \"TPNL\", \"ts\": \"2018-08-31 09:30:00\", \"month\": \"08\", \"high\": 3.38, \"low\": 3.38, \"key\": \"TPNL_2018-08-31 09\", \"year\": 2018, \"date\": \"2018/08/31\", \"close\": 3.37, \"open\": 3.37, \"day\": \"31\",\"add_clo\": \"heihei\"}",
+    "{\"volume\": 558465, \"symbol\": \"CGC\", \"ts\": \"2018-08-31 09:30:00\", \"month\": \"08\", \"high\": 44.58, \"low\": 44.2876, \"key\": \"CGC_2018-08-31 09\", \"year\": 2018, \"date\": \"2018/08/31\", \"close\": 44.29, \"open\": 44.5, \"day\": \"31\",\"add_clo\": \"wawa\"}")
     val df = spark.read.json(spark.sparkContext.parallelize(inserts, 2))
     df.write.format("hudi").
       //option(OPERATION_OPT_KEY,"delete").
       options(getQuickstartWriteConfigs).
       option(PRECOMBINE_FIELD_OPT_KEY, "ts").
-      option(RECORDKEY_FIELD_OPT_KEY, "uuid").
-      option(PARTITIONPATH_FIELD_OPT_KEY, "partitionpath").
+      option(RECORDKEY_FIELD_OPT_KEY, "key").
+      option(PARTITIONPATH_FIELD_OPT_KEY, "date").
       //option(HIVE_PARTITION_EXTRACTOR_CLASS_OPT_KEY,"").
       option(TABLE_NAME, tableName).
-      option(DataSourceWriteOptions.TABLE_NAME_OPT_KEY, "hudi_hive_test").
-      option(DataSourceWriteOptions.HIVE_SYNC_ENABLED_OPT_KEY, true).
+      option(DataSourceWriteOptions.TABLE_NAME_OPT_KEY,tableName).
+      option(DataSourceWriteOptions.HIVE_TABLE_OPT_KEY, tableName).
+      //option(DataSourceWriteOptions.HIVE_SYNC_ENABLED_OPT_KEY, true).
+      option(DataSourceWriteOptions.META_SYNC_ENABLED_OPT_KEY, true).
       option(DataSourceWriteOptions.HIVE_DATABASE_OPT_KEY, "default").
-      option(DataSourceWriteOptions.HIVE_TABLE_OPT_KEY, "hudi_hive_test").
       option(DataSourceWriteOptions.HIVE_USER_OPT_KEY, "hive").
       option(DataSourceWriteOptions.HIVE_PASS_OPT_KEY, "hive").
       option(DataSourceWriteOptions.HIVE_URL_OPT_KEY, "jdbc:hive2://192.168.16.181:10000").
-      //option(DataSourceWriteOptions.HIVE_PARTITION_FIELDS_OPT_KEY, "").
-      mode(SaveMode.Overwrite).
+      option(DataSourceWriteOptions.HIVE_PARTITION_FIELDS_OPT_KEY, "date").
+      option(DataSourceWriteOptions.HIVE_STYLE_PARTITIONING_OPT_KEY,true).
+      mode(SaveMode.Append).
       save(basePath)
 
 
@@ -63,9 +127,6 @@ object TestHudi2 {
     //load(basePath) use "/partitionKey=partitionValue" folder structure for Spark auto partition discovery
     tripsSnapshotDF.createOrReplaceTempView("hudi_trips_snapshot")
     println("first query===========>")
-    spark.sql("select fare, begin_lon, begin_lat, ts from  hudi_trips_snapshot where fare > 20.0").show()
-    println("second query===========>")
-    spark.sql("select _hoodie_commit_time, _hoodie_record_key, _hoodie_partition_path, rider, driver, fare from  hudi_trips_snapshot").show()
-
+    spark.sql("select * from  hudi_trips_snapshot ").show()
   }
 }
