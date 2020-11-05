2836b1018a628de64b33bca686247b08725620de
niceshot <wanqiufeng@outlook.com>
2020-11-05 21:44:58 +0800

    打印异常。

--- src/main/java/com/niceshot/hudi/bo/HudiHandleObject.java
+++ src/main/java/com/niceshot/hudi/bo/HudiHandleObject.java
@@ -9,10 +9,42 @@ import java.util.Map;
 /**
  * @author created by chenjun at 2020-10-23 18:35
  */
-@Data
 public class HudiHandleObject {
     private String database;
     private String table;
     private String operationType;
     private List<Map<String,String>> data;
+
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(String operationType) {
+        this.operationType = operationType;
+    }
+
+    public List<Map<String, String>> getData() {
+        return data;
+    }
+
+    public void setData(List<Map<String, String>> data) {
+        this.data = data;
+    }
 }
